59494fa1904b5a31aff3c02ad1e5d14cd6e402bb
Andrew Headrick <andrew.headrick@gmail.com>
2012-09-21 00:27:05 -0700

    did two out of three exercises for assignment one

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,12 +14,56 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    
+    def next( curC :Int, curR :Int, curRow :IndexedSeq[Int], prevRow :IndexedSeq[Int] ) :Int = {
+      def calc = 
+	      if ( prevRow.isDefinedAt(curR - 1) )
+	        prevRow( curR - 1 ) + prevRow(0)
+	      else 
+	        prevRow(0)
+      
+      if ( curR == r - 1 && curC == c ) 
+        calc
+      else if ( curC == curR )
+        next( 0, curR + 1, IndexedSeq.empty[Int], curRow :+ calc )
+      else
+        next( curC + 1, curR, curRow :+ calc, prevRow )
+    }
+    
+    if ( c == 0 || r == 0 || r == 1 )
+      1     
+    else if ( c == 1 || c + 1 - r == 0 )
+      r
+    else
+      next( 0, 0, IndexedSeq.empty[Int], IndexedSeq.empty[Int] )
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    
+    def calc( cnt :Int, remaining :List[Char] ) :Boolean = {
+      
+      def calcChar = remaining.head.toString match {
+        case "(" => 1 + cnt
+        case ")" => -1 + cnt
+        case _ => cnt
+      }
+      
+      if ( cnt < 0 )
+        false
+      else if ( remaining.isEmpty && cnt == 0 )
+        true
+      else if ( remaining.isEmpty && cnt != 0 )
+        false
+      else
+        calc( calcChar , remaining.tail )
+    }
+    
+    calc(0, chars)
+  }
 
   /**
    * Exercise 3
